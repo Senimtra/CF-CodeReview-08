a174779fd90d7e0334060e7b94da57490db20d23
Gregor Seegers <gregor.seegers@gmail.com>
2021-04-02 20:59:34 +0200

    add logo + basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
