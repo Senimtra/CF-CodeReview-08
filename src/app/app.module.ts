94c76da53933da4753dd311846c208a0070a7951
Gregor Seegers <gregor.seegers@gmail.com>
2021-04-03 17:18:30 +0200

    create navbar contact / github / linkedin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeadBarComponent } from './head-bar/head-bar.component';
 import { FooBarComponent } from './foo-bar/foo-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeadBarComponent, FooBarComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeadBarComponent,
+    FooBarComponent,
+    NavbarComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
