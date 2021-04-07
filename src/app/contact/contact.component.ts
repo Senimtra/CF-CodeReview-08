9a12cba75d7b8558852495ab5450f32c76152bec
Gregor Seegers <gregor.seegers@gmail.com>
2021-04-07 18:21:15 +0200

    add header link + remove scroll counter

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({
+      behavior: 'smooth',
+      // block: 'nearest',
+      // inline: 'start',
+    });
   }
 
+  ngOnInit(): void {}
 }
