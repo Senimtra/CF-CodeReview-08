809836c6672b56273ab2efb99b80236d6b68ff1a
Senimtra <senimtra@hotmail.com>
2021-06-02 07:20:25 +0200

    remove CodeReview 10 files

--- FSWDC_CodeReview_10/php/components/googlefont.php
+++
@@ -1,4 +0,0 @@
-<!-- ### Add Noto-Font from Google-fonts ### -->
-
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@1,700&display=swap" rel="stylesheet">
